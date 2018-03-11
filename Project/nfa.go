e6b73c8995407f9ba375aa6d2a7d92afbdab5dbc
yrrag5 <G00335806@gmit.ie>
2018-03-11 21:49:07 +0000

    Adding Thompson's construction algorithm

---
+++ Project/nfa.go
@@ -0,0 +1,72 @@
+package main
+
+import(
+	"fmt"
+)
+
+type state struct {
+	symbol rune
+	edge1 *state
+	edge2 *state
+}
+
+type nfa struct {
+	initial *state
+	accept *state
+}
+
+func poregtonfa(pofix string) *nfa {
+	nfastack := []*nfa{}
+
+	for _, r := range pofix {
+		switch r {
+		case '.':
+			frag2 := nfastack[len(nfastack)-1]
+			nfastack = nfastack[:len(nfastack)-1]
+			frag1 := nfastack[len(nfastack)-1]
+			nfastack = nfastack[:len(nfastack)-1]
+
+			frag1.accept.edge1 = frag2.initial
+
+			nfastack = append(nfastack, &nfa{initial: frag1.initial, accept: frag2.accept})
+		case '|':
+			frag2 := nfastack[len(nfastack)-1]
+			nfastack = nfastack[:len(nfastack)-1]
+			frag1 := nfastack[len(nfastack)-1]
+			nfastack = nfastack[:len(nfastack)-1]
+
+			initial := state{edge1: frag1.initial, edge2: frag2.initial}
+			accept := state{}
+			frag1.accept.edge1 = &accept
+			frag2.accept.edge1 = &accept
+
+			nfastack = append(nfastack, &nfa{initial: &initial, accept: &accept})
+
+
+		case '*':
+			frag := nfastack[len(nfastack)-1]
+			nfastack = nfastack[:len(nfastack)-1]
+
+			accept := state{}
+			initial := state{edge1: frag.initial, edge2: &accept}
+			frag.accept.edge1 = frag.initial
+			frag.accept.edge2 = &accept
+
+			nfastack = append(nfastack, &nfa{initial: &initial, accept: &accept})
+
+		
+		default:
+			accept := state{}
+			initial := state{symbol: r, edge1: &accept}
+
+			nfastack = append(nfastack, &nfa{initial: &initial, accept: &accept})
+		}
+	}
+
+	return nfastack[0]
+}
+
+func main() {
+	nfa := poregtonfa("ab.c*|")
+	fmt.Println(nfa)
+}
